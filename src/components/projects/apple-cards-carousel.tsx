1afae6d84f4c5f2c4893b5f72c75412ca505a2d8
Christopher Heher <chrisheher@gmail.com>
2025-12-04 23:41:13 -0800

    Your commit message

--- src/components/projects/apple-cards-carousel.tsx
+++ src/components/projects/apple-cards-carousel.tsx
@@ -158,8 +158,7 @@ export const Carousel = ({
                   transition: {
                     duration: 0.5,
                     delay: 0.2 * index,
-                    ease: 'easeOut',
-                    once: true,
+                  
                   },
                 }}
                 key={'card' + index}
